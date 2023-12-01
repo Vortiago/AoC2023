22156fb80b9ac5efe681b6545c27eeb3a286555a
Atle H. Havsø <atle@havso.net>
2023-12-01 18:17:38 +0100

    Applied Clean Code.

--- day1.ps1
+++ day1.ps1
@@ -5,11 +5,34 @@ function DefaultParser($inputText, $split = [System.Environment]::NewLine) {
     return $inputText.Split($split, $stringSplitOptions)
 }
 
+function TextNumberReplacer($inputText) {
+    $inputText = $inputText.Replace("one", "o1ne")
+    $inputText = $inputText.Replace("two", "t2wo")
+    $inputText = $inputText.Replace("three", "t3hree")
+    $inputText = $inputText.Replace("four", "f4our")
+    $inputText = $inputText.Replace("five", "f5ive")
+    $inputText = $inputText.Replace("six", "s6ix")
+    $inputText = $inputText.Replace("seven", "s7even")
+    $inputText = $inputText.Replace("eight", "e8ight")
+    $inputText = $inputText.Replace("nine", "n9ine")
+    $inputText = $inputText.Replace("zero", "z0ero")
+    return $inputText
+}
+
+function NumberCollector($inputText) {
+    $integers = $inputText.ToCharArray() | Where-Object { $_ -match "[0-9]" }
+    return $integers
+}
+
+function ConcatFirstAndLastDigit($integers) {
+    return $integers[0] + $integers[-1]
+}
+
 $s = DefaultParser($input)
 $outerSum = 0
 foreach($line in $s) {
-    $integers = $line.ToCharArray() | Where-Object { $_ -match "[0-9]" }
-    $outerSum += $integers[0] + $integers[-1]
+    $integers = NumberCollector($line)
+    $outerSum += ConcatFirstAndLastDigit($integers)
 }
 
 Write-Output "Problem 1: "$outerSum
@@ -25,18 +48,9 @@ zoneight234
 $s = DefaultParser($input)
 $outerSum2 = 0
 foreach($line in $s) {
-    $line = $line.Replace("one", "o1ne")
-    $line = $line.Replace("two", "t2wo")
-    $line = $line.Replace("three", "t3hree")
-    $line = $line.Replace("four", "f4our")
-    $line = $line.Replace("five", "f5ive")
-    $line = $line.Replace("six", "s6ix")
-    $line = $line.Replace("seven", "s7even")
-    $line = $line.Replace("eight", "e8ight")
-    $line = $line.Replace("nine", "n9ine")
-    $line = $line.Replace("zero", "z0ero")
-    $integers = $line.ToCharArray() | Where-Object { $_ -match "[0-9]" }
-    $outerSum2 += $integers[0] + $integers[-1]
+    $line = TextNumberReplacer($line)
+    $integers = NumberCollector($line)
+    $outerSum2 += ConcatFirstAndLastDigit($integers)
 }
 
 Write-Output "Problem 2: "$outerSum2
