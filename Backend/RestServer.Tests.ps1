dc2018334ec5c310df2027706d26f3be4f8f6d41
Atle H. Havsø <atle@havso.net>
2023-12-01 22:29:06 +0100

    Moved back to single test-file as I figured out powershell weirdness...

--- Backend/RestServer.Tests.ps1
+++ Backend/RestServer.Tests.ps1
@@ -1,9 +1,24 @@
-#using module "./RestServer.psm1"
+import-module $PSScriptRoot"/RestServer.psm1"
 
 Describe "RestServer" {
     It "Should return correct response!" {
-        Start-Job -FilePath $PSScriptRoot/RestServer.TestsBackend.ps1
+        Start-Job -ScriptBlock {
+            class Ping {
+                [string]ApiResponse($request) {
+                    return "Pong"
+                }
+            }
+
+            $server = [RestServer]::new()
+            $server.AddEndpoint([Ping]::new())
+            $server.Run()
+        }
+
         $response = Invoke-RestMethod -Uri "http://localhost:8000/ping"  
         $response | Should -Be "Pong" 
     }
+}
+
+AfterAll {
+    Remove-Module RestServer
 }
